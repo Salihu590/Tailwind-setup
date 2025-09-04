3c6e07a570eb27fe0e09a2b4f04dc6b13d7616aa
Salihu5906 <Suleimansalihu2007@gmail.com>
2025-09-04 17:40:47 +0100

    fixed dashboard.js

--- client/public/dashboard.js
+++ client/public/dashboard.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/api/admin";
+const API_URL = "https://tailwind-setup.onrender.com/api/admin";
 
 const app = document.getElementById("app");
 const loginFormContainer = document.getElementById("login-form-container");
